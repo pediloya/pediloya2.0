639b319fdd3bcbdb2d32885ce478594147fc6835
pediloya <pediloya.mingob@gmail.com>
2022-07-05 16:53:44 -0300

    missing useRef import in CrearPedidoInnerSomos.js

--- src/Components/CrearPedidoInnerSomos.js
+++ src/Components/CrearPedidoInnerSomos.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { Form, Button } from 'react-bootstrap'
 import { useAuth } from '../Context/AuthContext'
 import { useCrearPedido } from '../Context/CrearPedidoContext'
